b129f6d1565e44b82812249a28617681a70751e2
Tariq-Sekhri <hulkstar9999@gmail.com>
2023-10-26 15:15:45 -0400

    adding the Pieces enum

---
+++ src/main/java/com/chess_v2/beans/Pieces.java
@@ -0,0 +1,20 @@
+package com.chess_v2.beans;
+
+public enum Pieces {
+    King(1000000000),
+    Queen(9),
+    Rook(5),
+    Bishop(3),
+    Knight(3),
+    pawn(1);
+
+    private int value;
+    private Pieces (int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+}
